0fbf710fbaaae3f102a90fae860481debd1b7fc0
Aviv-lab <Aviv.nadav09@gmail.com>
2026-01-28 16:04:49 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -234,14 +234,20 @@ public class Queue_Base_Sort
     }
 }
 ----------------------------------------------------------------------------
-	import java.util.*;
+import java.util.*;
 public class BinNode_Homework
 {
     public static void main(String[] args) {
 		BinNode<Integer> t15 = new BinNode<Integer>(15);
 		BinNode<Integer> t14 = new BinNode<Integer>(14);
 		BinNode<Integer> t9 = new BinNode<Integer>(9);
+		BinNode<Integer> t3 = new BinNode<Integer>(3);
         t14.setLeft(t9);
+		t14.setRight(t3);
+		BinNode<Integer> t2 = new BinNode<Integer>(2);
+		BinNode<Integer> t1 = new BinNode<Integer>(1);
+		t1.setLeft(t3);
+		t1.setRight(t2);
         BinNode<Integer> tree = new BinNode<Integer>(t15, 3, t14);
 		//tree.getLeft().setLeft(new BinNode<Integer>(t9, 6, new BinNode<Integer>(7)));
 		//tree.getLeft().setRight(new BinNode<Integer>(new BinNode<Integer>(34), 56, new BinNode<Integer>(12)));
@@ -257,10 +263,14 @@ public class BinNode_Homework
         
         //ex1And2(tree);
 		//ex3(tree);
-        Node<Integer> lst = ex5(tree);
-        System.out.println(lst);
-        
-
+        //Node<Integer> lst = ex5(tree);
+        //System.out.println(lst);
+        tree.display();
+		System.out.println(treeHeight(tree));
+		System.out.println(isBalanced(tree));
+		System.out.println(leafCount(tree));
+		System.out.println(allIsIn(tree, t14));
+        System.out.println(sequentialTreeN(t1, 3));
 	}
 
 	/* Construct from in-order with brackets */
@@ -619,4 +629,75 @@ public class BinNode_Homework
         }
         return head;
     }
+
+    public static <T> int treeHeight(BinNode<T> t)
+	{
+		if (t==null)
+			return -1;
+		return 1+Math.max(treeHeight(t.getLeft()), treeHeight(t.getRight()));
+	}
+
+	public static boolean isBalanced(BinNode<Integer> t)
+	{
+		if (t==null)
+			return true;
+		if (Math.abs(treeHeight(t.getLeft())-treeHeight(t.getRight()))>1)
+			return false;
+		return isBalanced(t.getLeft()) && isBalanced(t.getRight());
+	}
+
+	public static int leafCount(BinNode<Integer> t)
+	{
+		if (t==null)
+			return 0;
+		if (!t.hasLeft()&&!t.hasRight())
+			return 1;
+		return leafCount(t.getLeft())+leafCount(t.getRight());
+	}
+
+	public static boolean isIn(BinNode<Integer> t, int a)
+	{
+		if (t==null)
+			return false;
+		if (t.getValue()==a)
+			return true;
+		return isIn(t.getLeft(), a) || isIn(t.getRight(), a);
+	}
+
+	public static boolean allIsIn(BinNode<Integer> t1, BinNode<Integer> t2)
+	{
+		if (t2==null)
+			return true;
+		if (!isIn(t1, t2.getValue()))
+			return false;
+		return allIsIn(t1, t2.getLeft()) && allIsIn(t1, t2.getRight());
+	}
+
+	public static int nodeCount(BinNode<Integer> t)
+	{
+		if (t==null)
+			return 0;
+		return 1+ nodeCount(t.getLeft())+nodeCount(t.getRight());
+	}
+
+	public static int isInCount(BinNode<Integer> t, int a)
+	{
+		int b=0;
+		if (t==null)
+			return 0;
+		if (t.getValue()==a)
+			b++;
+		return b + isInCount(t.getLeft(), a) + isInCount(t.getRight(), a);
+	}
+
+	public static boolean sequentialTreeN(BinNode<Integer> t, int n)
+	{
+		for (int i=1; i<=n; i++)
+		{
+			if (isInCount(t, i)!=1)
+				return false;
+		}
+		return true;	
+	}
+
 }
